17e5ae90a767d10e20583fdc794645fcd08e9d42
Jelson J <jelson20@outlook.com>
2022-01-07 13:38:39 +0000

    commit background image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,29 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "static-image",
-  },
-  plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-transformer-remark",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+	siteMetadata: {
+		siteUrl: 'https://www.yourdomain.tld',
+		title: 'static-image'
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-sharp',
+		'gatsby-plugin-sitemap',
+		'gatsby-plugin-image',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				icon: 'src/images/icon.png'
+			}
+		},
+		'gatsby-plugin-mdx',
+		'gatsby-transformer-remark',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'pages',
+				path: './src/pages/'
+			},
+			__key: 'pages'
+		}
+	]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import * as React from 'react';
+import * as styles from './styles.module.css';
 import { StaticImage } from 'gatsby-plugin-image';
 // markup
+
 const IndexPage = () => {
 	return (
-		<div className='app'>
+		<div className={styles.container}>
 			<h1>Gatsby Static image</h1>
-			<StaticImage src='' />
+			<StaticImage
+				className={styles.avatar}
+				alt='Avatar'
+				style={{ position: 'absolute' }}
+				src='https://images.pexels.com/photos/237272/pexels-photo-237272.jpeg'
+				//src='images/fast-furios.jpg'
+			/>
 		</div>
 	);
 };
